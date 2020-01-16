00aa24fc108252a38b8bb41fcb2dde02d2d1afea
DylanYoung00 <59969031+DylanYoung00@users.noreply.github.com>
2020-01-16 10:36:11 -0600

    added my major
    Added a line where I printed my major on visual studio code that then showed up on github

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+Dylan Young
+January 16, 2020
+Source Code for Hack 1
+*/
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Dylan Young");
+    printf("Computer Science\n");
+    return 0;
+}
